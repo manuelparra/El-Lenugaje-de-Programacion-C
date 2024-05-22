b5e8a025a970659246e24a59cc35ca573e49f8df
Manuel Parra <manuelparra@live.com.ar>
2024-05-22 11:00:36 +0100

    Exercise 1-7 done.

--- 01-Capitulo-Introduccion/EJ-1-7.c
+++ 01-Capitulo-Introduccion/EJ-1-7.c
@@ -2,4 +2,12 @@
  * Ejercicio 1-7. Escriba un programa que imprima el valor de EOF.
  */
 
+#include <stdio.h>
+
+int main() {
+
+	printf("El valor de EOF es: %d\n", EOF); // imprime el valor de EOF -> -1
+
+	return 0;
+}
 
