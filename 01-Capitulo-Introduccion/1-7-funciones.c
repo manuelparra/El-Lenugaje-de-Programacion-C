935938b015e250704127dcf70faf4d426fc77a7e
Manuel Parra <manuelparra@live.com.ar>
2024-06-01 14:28:39 +0200

    fixed some ussues in 1-7-funciones.c

--- 01-Capitulo-Introduccion/1-7-funciones.c
+++ 01-Capitulo-Introduccion/1-7-funciones.c
@@ -9,7 +9,7 @@ int main() {
 
 	for (i = 0; i < 10; ++i)
 		printf("%d %d %d\n", i, power(2, i), power(-3, i));
-	
+
 	return 0;
 }
 
@@ -17,7 +17,7 @@ int main() {
 
 int power(int base, int n) {
 	int i, p;
-	
+
 	p = 1;
 
 	for (i = 1; i <= n; i++)
